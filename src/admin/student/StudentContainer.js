7532f915ce963285b42d12f82961c284532938bf
Minh Tang Ba <tangbamiinh@gmail.com>
2021-03-07 18:37:16 +0700

    Changed pagination fetching logic to adapt to zero index at backend

--- src/admin/student/StudentContainer.js
+++ src/admin/student/StudentContainer.js
@@ -17,48 +17,48 @@ const StudentContainer = () => {
     current: 1,
     pageSize: 10,
   });
-  const [filters, setFilters] = useState({
-    // need to change first requet with params ?page=1
-    page: 0,
-    size: 10
-  })
-  useEffect(() => {
-    async function fetchStudentList() {
-      try {
 
-        //calling getStudents() function from student action 
-        // => got undefine dispite of being connected mapDispatchToProps
-        // => will refactor code using redux later
-        const paramsString = queryString.stringify(filters);
-        const data = await studentApi.getPaginatedStudents(paramsString);
-        const { content, pageable, totalElements } = data;
+  async function fetchStudentList(antCurrentPage, pageSize) {
+    try {
 
-        const studentsToDisplay = content.map((student) => {
-          const { username, lastName, firstName, gender, email, sinceYear, department, phoneNumber, fullAddress} = student;
-          return {
-            key: uuidv4(),
-            studentID: username,
-            fullName: `${lastName} ${firstName}`,
-            gender: gender.toLowerCase(),
-            email,
-            sinceYear,
-            department: department.code,
-            phoneNumber,
-            fullAddress
-          }
-        });
-        setStudentData(studentsToDisplay);
-        setPagination({
-          current: pageable.pageNumber,
-          pageSize: pageable.pageSize,
-          total: totalElements,
-        })
-      } catch (error) {
-          console.log(error)
-      }
+      //calling getStudents() function from student action 
+      // => got undefine dispite of being connected mapDispatchToProps
+      // => will refactor code using redux later
+      const paramsString = queryString.stringify({
+        page: antCurrentPage - 1,
+        size: pageSize,
+      });
+      const data = await studentApi.getPaginatedStudents(paramsString);
+      const { content, pageable, totalElements } = data;
+
+      const studentsToDisplay = content.map((student) => {
+        const { username, lastName, firstName, gender, email, sinceYear, department, phoneNumber, fullAddress} = student;
+        return {
+          key: uuidv4(),
+          studentID: username,
+          fullName: `${lastName} ${firstName}`,
+          gender: gender.toLowerCase(),
+          email,
+          sinceYear,
+          department: department.code,
+          phoneNumber,
+          fullAddress
+        }
+      });
+      setStudentData(studentsToDisplay);
+      setPagination({
+        current: pageable.pageNumber + 1,
+        pageSize: pageable.pageSize,
+        total: totalElements,
+      })
+    } catch (error) {
+        console.log(error)
     }
-    fetchStudentList()
-  },[filters]);
+  }
+
+  useEffect(() => {
+    fetchStudentList(pagination.current, pagination.pageSize)
+  },[]);
 
     // Search function
   const handleSearch = searchText => {
@@ -71,11 +71,8 @@ const StudentContainer = () => {
 
   const handleTableChange = (pagination) => {
     const { current, pageSize } = pagination;
-    setFilters({
-      ...filters,
-      page: current,
-      size: pageSize,
-    })
+    // setPagination(pagination);
+    fetchStudentList(current, pageSize);
   }
   return (
     <div>
