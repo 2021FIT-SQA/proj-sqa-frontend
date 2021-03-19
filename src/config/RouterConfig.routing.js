d6f6093981cd0a723eac090894aa0c3b69e3c3ef
Minh Tang Ba <tangbamiinh@gmail.com>
2021-03-19 18:05:48 +0700

    loading behaviour before authentication added

--- src/config/RouterConfig.routing.js
+++ src/config/RouterConfig.routing.js
@@ -1,54 +1,93 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-// LAYOUT 
-import MainAdmin from 'layout/main-admin/MainAdmin'
-import Main from 'layout/main/Main'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// LAYOUT
+import MainAdmin from "layout/main-admin/MainAdmin";
+import Main from "layout/main/Main";
 
-// COMPONENT 
-import LoginPage from 'shared/login/LoginComponent'
-import LandingPage from 'layout/landing/LandingComponent'
-import UserDashboardPage from 'end-user/UserDashboard'
-import PrivateRoute from 'config/privateRoute.config'
+// COMPONENT
+import LoginPage from "shared/login/LoginComponent";
+import LandingPage from "layout/landing/LandingComponent";
+import UserDashboardPage from "end-user/UserDashboard";
+import PrivateRoute from "config/privateRoute.config";
+import { Spin } from "antd";
 
-import { 
+import { connect } from "react-redux";
+
+import {
   AdminDashboardPage,
   AdminCoursePage,
   AdminDepartmentPage,
   AdminEnrollmentPage,
   AdminStudentPage,
-  AdminTeacherPage } from 'admin'
+  AdminTeacherPage,
+} from "admin";
 
-const RouterConfig = () => {
+const RouterConfig = ({ loading }) => {
+  if (loading)
+    return (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <Spin size="large" />
+        </div>
+      </div>
+    );
+
   return (
     <Router>
       <Switch>
-        <Route path='/login' component={LoginPage} />
-        <Route path='/' exact component={LandingPage} />
-        <Route path='/admin/:path?' exact>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/admin/:path?" exact>
           <MainAdmin>
             <Switch>
-              <PrivateRoute path='/admin' exact component={AdminDashboardPage} />
-              <PrivateRoute path='/admin/students' component={AdminStudentPage} />
-              <PrivateRoute path='/admin/teachers' component={AdminTeacherPage} />
-              <PrivateRoute path='/admin/courses' component={AdminCoursePage} />
-              <PrivateRoute path='/admin/departments' component={AdminDepartmentPage} />
-              <PrivateRoute path='/admin/enrollments' component={AdminEnrollmentPage} />
+              <PrivateRoute
+                path="/admin"
+                exact
+                component={AdminDashboardPage}
+              />
+              <PrivateRoute
+                path="/admin/students"
+                component={AdminStudentPage}
+              />
+              <PrivateRoute
+                path="/admin/teachers"
+                component={AdminTeacherPage}
+              />
+              <PrivateRoute path="/admin/courses" component={AdminCoursePage} />
+              <PrivateRoute
+                path="/admin/departments"
+                component={AdminDepartmentPage}
+              />
+              <PrivateRoute
+                path="/admin/enrollments"
+                component={AdminEnrollmentPage}
+              />
             </Switch>
           </MainAdmin>
         </Route>
-        
-        <Route path='user/:path?' exact>
+
+        <Route path="user/:path?" exact>
           <Main>
             <Switch>
-              <Route path='/user' exact component={UserDashboardPage} />
+              <Route path="/user" exact component={UserDashboardPage} />
             </Switch>
           </Main>
         </Route>
-
-
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-export default RouterConfig
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  loading: state.auth.loading,
+});
+
+export default connect(mapStateToProps, null)(RouterConfig);
